5432e8c0251471c49e9b01100245a65f85a3c388
Marcin Cholewka <Marcin@DESKTOP-QC1DUAN>
2021-05-05 22:54:46 +0200

    Create question get methods inside contollers

--- controllers/questionController.ts
+++ controllers/questionController.ts
@@ -4,14 +4,38 @@ const { pool } = require('../database/db');
 
 const questionController = router();
 
-questionController.post('/create', async (req, res) => {
+questionController.get('/', async (req, res) => {
+    try {
+        const questions = await pool.query("SELECT * FROM question");
+
+        res.json(questions);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+questionController.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const questions = await pool.query("SELECT * FROM question WHERE question_id = $1", [id]);
+
+        console.log(questions);
+
+        res.json(questions.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+questionController.post('/', async (req, res) => {
     try {
         const { title, content } = req.body;
         const newQuestion = await pool.query("INSERT INTO question (title, content) VALUES ($1, $2) RETURNING *", [title, content]);
 
         res.json(newQuestion);
-    } catch (err) {
-        console.error(err.message);
+    } catch (error) {
+        console.error(error.message);
     }
 });
 
