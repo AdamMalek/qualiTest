698ea3b5260d85e6acb1ea22172c3ba7e2a28c2e
Marcin Cholewka <Marcin@DESKTOP-QC1DUAN>
2021-05-12 23:54:26 +0200

    Remove slugs controller

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 require('dotenv').config();
 import express from "express";
-import slugsController from "./controllers/slugsController";
 import questionsController from "./controllers/questionsController";
 import { requestLoggerMiddleware } from "./middlewares/requestLoggerMiddleware";
 
@@ -17,7 +16,6 @@ app.use(express.json(), requestLoggerMiddleware);
 app.all("/echo", (req, res) => res.json({ query: req.query, body: req.body }));
 
 const api = express.Router()
-  .use('/slugs', slugsController)
   .use('/questions', questionsController);
 
 app.use("/api", api);
