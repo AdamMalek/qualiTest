7112600888c1a47744495f4408cc6da27047be29
Marcin Cholewka <Marcin@DESKTOP-QC1DUAN>
2021-05-14 00:48:16 +0200

    Clear database.ts/migrate.ts files

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 export default {
     user:  process.env.DATABASE_USER as string,
     password: process.env.DATABASE_PASSWORD as string,
--- database/migrate.ts
+++ database/migrate.ts
@@ -4,8 +4,6 @@ import dbConfig from "../config/database";
 async function runMigrations(): Promise<void> {
     console.log("Migrations has been started.");
 
-    console.log(dbConfig);
-
     await createDb('quali_test', {
         ...dbConfig,
         defaultDatabase: "postgres", // defaults to "postgres"
